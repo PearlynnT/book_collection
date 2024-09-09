f2addbbb5844e8ae84b55e9a4008c0c73cb0945b
Pearlynn <98682935+PearlynnT@users.noreply.github.com>
2024-09-08 21:58:17 -0500

    add book collection 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :user_books
+  resources :users
+  # resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'books#index'
+  # root 'books#index'
+  root 'user_books#index'
 
   resources :books do
     member do
